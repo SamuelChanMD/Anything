d20d666f18247a9ffe825503f2e0328af8cd0cec
Samuel Chan <samuelchanmd@gmail.com>
2015-01-31 12:00:11 -0500

    Added a comment for newMethod.

--- src/Whatevs.java
+++ src/Whatevs.java
@@ -7,6 +7,7 @@ public class Whatevs {
 	System.out.println("Git and SourceTree are both getting a little confusing.");
 	}
 	
+	//This is a new method. 
 	public void newMethod(){
 		int i = 3;
 		if(i == 3){
