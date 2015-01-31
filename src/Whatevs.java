07c0000ed0b7d9a1cd9a54716acf16e1f945483f
Samuel Chan <samuelchanmd@gmail.com>
2015-01-31 11:46:59 -0500

    I guess this is my develop branch?

--- src/Whatevs.java
+++ src/Whatevs.java
@@ -4,6 +4,7 @@ public class Whatevs {
 	public static void main(String[] args){
 	System.out.println("Hello World!");
 	System.out.println("Hello Samuel!");
+	System.out.println("Git and SourceTree are both getting a little confusing.");
 	
 	}
 	
