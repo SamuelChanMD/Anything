a6c1fb35eec45c936bec2c8052db2beb7f66af54
Samuel Chan <samuelchanmd@gmail.com>
2015-01-31 11:50:36 -0500

    I hoper there is no error on this code.

--- src/Whatevs.java
+++ src/Whatevs.java
@@ -5,7 +5,7 @@ public class Whatevs {
 	System.out.println("Hello World!");
 	System.out.println("Hello Samuel!");
 	System.out.println("Git and SourceTree are both getting a little confusing.");
-	
+	my friend told me to make an error; so here it is.
 	}
 	
 }
